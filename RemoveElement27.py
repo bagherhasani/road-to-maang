0348e24db6385a447e6e331551bba29573d20394
bagherhasani0 <bah3944@utulsa.edu>
2026-04-09 15:38:10 -0500

    Solved: 27 with for each - with backward loop

--- RemoveElement27.py
+++ RemoveElement27.py
@@ -5,15 +5,24 @@ class Solution(object):
     # ! we cannot go over orignal and remove item because as we delete items the iteration will be wrong 
     
 
-
+    #for each loop
     def removeElement(self, nums, val):
-        
-        for i in range(len(nums)):
-           if nums[i]==val:
-               nums.remove(nums[i])
-               print(f"i:{i}, value:{nums[i]}")
+        for item in nums[:]:
+            if item== val:
+                nums.remove(item)
+        return len(nums)
+    
+
+    #We go backward meaning we visit from right to left. 
+    # And removals happen on the right side which we already visited. So it doesn't matter if it shifts.
+
+    def removeElement2(self,nums,val):
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i]==val:
+                nums.remove(nums[i])
 
         return len(nums)
+
         
        
 
@@ -29,8 +38,11 @@ class Solution(object):
 if __name__=="__main__":
     print("main")
 
-    nums=[0,1,2,2,3,0,4,2]
+    nums=[0,1,2,2]
     val=2
 
     sol=Solution()
-    sol.removeElement(nums,val)
+    print(sol.removeElement(nums,val))
+
+   
+    print(sol.removeElement2(nums,val))
