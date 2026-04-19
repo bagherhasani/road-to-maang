8f62fa7b4a73145a3c399a4625f3cdd1ed2ea857
bagherhasani0 <bah3944@utulsa.edu>
2026-04-18 19:48:12 -0500

    Solved: H-index Q274

---
+++ H-index274.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def hIndex(self, citations):
+         def hIndex(self, citations):
+            citations.sort(reverse=True)
+            h = 0
+            counter=0
+
+            for i in range(len(citations)):
+                    counter+=1
+                    if citations[i]>=counter:
+                        h=counter
+
+            return h
+             
+
+
+
+if __name__=="__main__":
+
+    citations=[3, 0, 6, 1, 5]
+    citations=[1,3,1]
+
+    citations.sort(reverse=True)
+    h=0
+
+    counter=0
+
+    for i in range(len(citations)):
+            counter+=1
+            if citations[i]>=counter:
+                h=counter
+
+    print(h)
+
